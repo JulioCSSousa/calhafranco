81845154392503598a8ff3e08eb31379643d45eb
AmandaDev25 <amandadev3225@gmail.com>
2025-03-25 14:51:05 -0300

    fix google free button

--- src/app/components/GoogleViews.tsx
+++ src/app/components/GoogleViews.tsx
@@ -10,8 +10,9 @@ export default function     GoogleReviews() {
   }, []);
 
   return (
-    <div className="w-full py-10">
-      <div className="elfsight-app-28a1cd3c-c90f-479e-b5b0-7d27ab264c9c"></div>
+     <div style={{position: 'relative'}}>
+      <div className="elfsight-app-28a1cd3c-c90f-479e-b5b0-7d27ab264c9c" style={{zIndex: 0}}></div>
+      <div style={{position: 'absolute', right:'0px', bottom:'5px',zIndex: 10, height:'40px', width:'200px', backgroundColor:'white'}}></div>
     </div>
   );
 }
