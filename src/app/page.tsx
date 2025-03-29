cea4cc9e4d4603800aa636704128bfc5cc69a29e
Júlio C. S. Sousa <julio.sousa1993@gmail.com>
2025-03-29 13:12:36 -0300

    fix title onde estamos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -191,7 +191,7 @@ export default function Home() {
 
 
       <section className="p-8 text-center">
-        <h2 className="text-center mb-4">Onde estamos</h2>
+      <h2 className="section-title mb-4 text-center">Onde estamos</h2>
         <MapViewWrapper />
       </section>
 
