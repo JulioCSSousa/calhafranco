996e4cb9b9a50b202ab211dec792c3d423b4148a
Júlio C. S. Sousa <julio.sousa1993@gmail.com>
2025-04-01 22:16:22 -0300

    carousels and google review

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  domains: ['lh3.googleusercontent.com'],
 };
 
 export default nextConfig;
